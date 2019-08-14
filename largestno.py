fc4ff632c547c245ccbb1b37302e6e5c04dd73cd
KRRaji21 <54089103+KRRaji21@users.noreply.github.com>
2019-08-14 17:17:45 +0530

    Add files via upload

---
+++ largestno.py
@@ -0,0 +1,15 @@
+int num1=int(input())
+int num2=int(input())
+int num3=int(input())
+if(num1>num2)&(num1>num3):
+        {
+                print(num1)        }
+        elif(num2>num1)&(num2>num3):
+        {
+                print(num2)
+        }
+        else:
+             
+        {
+               print(num3)
+        }
